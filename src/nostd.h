b76553c11c24faa79bb2336864db5e68e16b1ce5
Sigmw <89428643+Sigmw@users.noreply.github.com>
2022-05-22 13:51:23 -0300

    rewriting strcmp (#5)

--- src/nostd.h
+++ src/nostd.h
@@ -1,25 +1,14 @@
 #ifndef __NES_DIFF_NO_STD
 #define __NES_DIFF_NO_STD
 
-int strcmp(const char* buffer1, const char* buffer2);
+int strcmp(const char *l, const char *r)
 
-int strcmp(const char* buffer1, const char* buffer2)
+int strcmp(const char *l, const char *r)
 {
-    register int compare_buffers;
-
-    while (*buffer1 && *buffer2)
-    {
-        compare_buffers = (*buffer1) - (*buffer2);
-
-        if (compare_buffers != 0) {
-            return compare_buffers;
-        }
-
-        if (*buffer1) buffer1++;
-        if (*buffer2) buffer2++;
+    while(*l==*r && *l) {
+        l++;
+        r++;
     }
-
-    return 0;
+	return *(unsigned char *)l - *(unsigned char *)r;
 }
-
 #endif
